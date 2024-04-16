f7f8d10a3705c4a9cff1e186477952a77cc79086
Sergio <sergut18@gmail.com>
2024-04-16 08:20:57 -0500

    disabled relative numbers and add swagger support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
+vim.g.autoformat = true
 
 vim.o.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
 
@@ -10,3 +10,5 @@ vim.cmd([[
   let g:conform_format_cmd = 'prettier'
   let g:conform_format_args = '--single-quote --parser babel'
 ]])
+
+vim.wo.relativenumber = false
---
+++ lua/plugins/swagger.lua
@@ -0,0 +1,4 @@
+return {
+  "xavierchow/vim-swagger-preview"
+  -- vim.keymap.set("n", "<leader>ws", "<Plug>(GenerateDiagram)")
+}
